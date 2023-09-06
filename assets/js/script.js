7b6c9cabf437a5118e712ae0ff2da901d9c7a46a
giacomobranchi <branchigiacomo28@gmail.com>
2023-09-06 09:22:27 +0200

    add countdown to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,30 @@
 //Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
 //Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+
+
+const lezione = new Date(2023, 8, 6, 9, 30, 0, 0);
+//document.getElementById('date').innerHTML = lezione;
+
+let tic = setInterval(function () {
+    let now = new Date().getTime();
+    //console.log(now);
+    let remaining = lezione - now;
+    //console.log(remaining);
+    let seconds = Math.floor((remaining % (1000 * 60 * 60 * 24)) / 1000);
+    document.getElementById("date").innerHTML = seconds;
+}, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Math.floor((remaining % (1000 * 86.400)) / 1000); Math.floor((remaining % (1000 * 60)) / 1000);
